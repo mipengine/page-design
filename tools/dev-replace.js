4fdd01342b5f6af3199ceb2ac5313e4ddfc40d39
xuexb <fe.xiaowu@gmail.com>
2017-12-26 22:34:51 +0800

    fix: #7

---
+++ tools/dev-replace.js
@@ -0,0 +1,24 @@
+/**
+ * @file 本地开发环境部署
+ * @author mip-support@baidu.com
+ */
+
+'use strict';
+
+const through2 = require('through2');
+
+module.exports = options => {
+    options = options || {};
+
+    return through2.obj((file, enc, cb) => {
+        if (file.isNull() || process.env.NODE_ENV !== 'development') {
+            cb(null, file);
+            return;
+        }
+
+        // 替换本地api路径
+        const reg = /https:\/\/www\.mipgo\.org\/html\/(www|templates|components)\/([\w\-]+)\/api\/([\w\-]+)\.json/g;
+        file.contents = new Buffer(file.contents.toString().replace(reg, './api/$3.json'));
+        cb(null, file);
+    });
+};
