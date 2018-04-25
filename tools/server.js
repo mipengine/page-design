e6c7d4182540c6872c812636823497597188bac7
xuexb <fe.xiaowu@gmail.com>
2018-04-25 20:07:05 +0800

    chore: 添加缓存时间

--- tools/server.js
+++ tools/server.js
@@ -15,11 +15,21 @@ app.use(json());
 
 // 注入
 app.use(api({
-    root: __dirname
+    // 接口中间件目录
+    root: __dirname,
+
+    // 添加接口跨域
+    process(req, res, next) {
+        res.set('Access-Control-Allow-Origin', '*');
+        res.set('Cache-Control', 'no-cache');
+    }
 }));
 
 // 托管静态文件
-app.use(express.static('./'));
+app.use(express.static('./', {
+    // 1小时过期时间
+    maxAge: '1h'
+}));
 
 // 百度BAE必须绑定这个端口 18080
 app.listen(18080, () => console.log('mipx app listening on port 18080!'));
